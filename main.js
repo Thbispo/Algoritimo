44a3321bcf70571d70e538cee2c61388deb0133b
Thbispo <thiagosouza0100@gmail.com>
2024-05-23 11:57:27 -0300

    Aulas sobre operadores lógicos

---
+++ main.js
@@ -0,0 +1,60 @@
+// aqui eu tenho um comentario
+console.log("Olá Mundo")
+
+var nome ="Tb"
+console.log ("Imprimir nome: ")
+console.log(nome)
+
+//Texto-> String
+//numeros inteiros -int
+//numeros decimais-> Float / Double
+//logicos-> Boolean)(false : True)
+
+var telefone = "19-99999-9999"
+var valormonetario = 77.77
+var valorinteiro = 12
+var verdadeirofalso = false
+
+// Guarde os dados do seu endereco em VARIAVEIS (ex. rua ,bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data nascimento) EM VARIAVEIS
+// Crie uma VARIAVEL que diz que voce é maior de idade
+// Crie uma VARIAVEL que diz se voc~e pode dirigir
+
+var nome = "Antonio Giordano"
+console.log("Nome da rua:" ,nome)
+
+var nome = "Nova Carioba"
+console.log("Nome do bairro:" ,nome)
+var nome = "13473-729"
+console.log("nome do cep:" ,nome)
+var nome = "São Paulo"
+console.log("nome do estado:" ,nome)
+
+var nome = "Thiago Bispo Souza"
+console.log("meu nome:" ,nome)
+var nome = "thiagosouza0100@gmail.com"
+console.log("meu email:" ,nome)
+var nome ="19 982866529"
+console.log("Celular:" ,nome )
+var nome = "24/03/2007"
+console.log("Data de Nascimento:" ,nome)
+
+var nome ="Não"
+console.log ("Posso dirigir:" ,nome)
+var nome = "Não"
+console.log ("Sou de maior:" ,nome)
+
+
+
+var numero8=4
+console.log("Tabuada do 4") 
+console.log("4x1 =:",numero8*1)
+console.log("4x2 =:",numero8*2)
+console.log("4x3 =:",numero8*3)
+console.log("4x4=:",numero8*4)
+console.log("4x5=:",numero8*5)
+console.log("4x6 =:",numero8*6)
+console.log("4x7=:",numero8*7)
+console.log("4x8 =:",numero8*8)
+console.log("4x9 =:",numero8*9)
+console.log("4x10 =:",numero8*10)
