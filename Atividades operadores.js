767e07f8efb4ad6c6ae44c9d9a2d1d515fed58b5
Thbispo <thiagosouza0100@gmail.com>
2024-05-24 09:46:14 -0300

    Atividade Operadores feita no dia 24/05

---
+++ Atividades operadores.js
@@ -0,0 +1,55 @@
+// Aqui eu tenho um comentario
+console.log("Atividade 1")
+
+var numero1,numero2
+var numero1= 20
+var numero2= 6
+
+console.log("========")
+ console.log("20+6=",numero1+numero2)
+ console.log("20x6=",numero1*numero2)
+ console.log("20/6=",numero1/numero2)
+ console.log("20-6=",numero1-numero2)
+ console.log("")
+ console.log("")
+
+// converter fahrenheit em celsius
+// converter celsius em fahrenheit
+
+var fahrenheit,celsius
+
+fahrenheit = 102   
+celsius = 36.9
+
+var comvercaoDofahrenheit = fahrenheit-32
+var comvercaoDocelsius = celsius*1.8
+
+console.log("Atividae 2 e 3")
+console.log("==========")
+console.log("2- 102 graus fahrenheit em celsius =",comvercaoDofahrenheit/1.8)
+console.log("3- 36.9 graus celsius em fahrenheit =",comvercaoDocelsius+32)
+console.log("")
+console.log("")
+
+
+
+
+// converter metros em centimetros
+ var Metros = 100
+ var metrosEmcentimeto = Metros * 100
+
+console.log(" Atividade 4")
+console.log("========")
+console.log("100 Metros em centimetros =",metrosEmcentimeto,"centimetros")
+console.log("")
+console.log("")
+
+// dar um desconto em um valor de produto
+var BolaDeFutebol = 129.99
+var desconto = BolaDeFutebol%40
+
+console.log(" Atividade 5")
+console.log("========")
+console.log("O valor do produto com desconto e de",BolaDeFutebol-desconto)
+console.log("")
+console.log("")
